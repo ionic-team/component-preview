033ddecd86f33364c180fc9df9af7a1b562bba1f
jgw96 <jgw9617@gmail.com>
2018-06-12 14:07:48 -0500

    feat(): add searchbar page

---
+++ src/components/searchbar/searchbar.tsx
@@ -0,0 +1,101 @@
+import { Component, State } from '@stencil/core';
+
+
+@Component({
+  tag: 'page-searchbar',
+  styleUrl: 'searchbar.css'
+})
+export class PageSearchbar {
+
+  @State() items;
+
+  constructor() {
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+      'Buenos Aires',
+      'Cairo',
+      'Dhaka',
+      'Edinburgh',
+      'Geneva',
+      'Genoa',
+      'Glasglow',
+      'Hanoi',
+      'Hong Kong',
+      'Islamabad',
+      'Istanbul',
+      'Jakarta',
+      'Kiel',
+      'Kyoto',
+      'Le Havre',
+      'Lebanon',
+      'Lhasa',
+      'Lima',
+      'London',
+      'Los Angeles',
+      'Madrid',
+      'Manila',
+      'New York',
+      'Olympia',
+      'Oslo',
+      'Panama City',
+      'Peking',
+      'Philadelphia',
+      'San Francisco',
+      'Seoul',
+      'Taipeh',
+      'Tel Aviv',
+      'Tokio',
+      'Uelzen',
+      'Washington'
+    ];
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  render() {
+    return (
+      <ion-app>
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>Searchbar</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+          <ion-searchbar onIonInput={(event) => this.getItems(event)}></ion-searchbar>
+          <ion-list>
+            {
+              this.items.map((item) => {
+                return (
+                  <ion-item>
+                    <ion-label>
+                      {item}
+                    </ion-label>
+                  </ion-item>
+                )
+              })
+            }
+          </ion-list>
+        </ion-content>
+      </ion-app>
+    );
+  }
+}
