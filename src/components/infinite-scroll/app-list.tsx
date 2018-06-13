7ae189e617c0840797d0f7602a45a28a09c6cd8e
jgw96 <jgw9617@gmail.com>
2018-06-13 11:53:26 -0500

    feat(): add infinite scroll page

---
+++ src/components/infinite-scroll/app-list.tsx
@@ -0,0 +1,26 @@
+import { Component, Prop } from '@stencil/core';
+
+
+@Component({
+  tag: 'app-list'
+})
+export class AppList {
+
+  @Prop() items: any[];
+  
+  render() {
+    return (
+      <ion-list>
+        {
+          this.items.map((item) => {
+            return (
+              <ion-item>
+                {item}
+              </ion-item>
+            )
+          })
+        }
+      </ion-list>
+    );
+  }
+}
---
+++ src/components/infinite-scroll/infinite-scroll.tsx
@@ -0,0 +1,52 @@
+import { Component, State } from '@stencil/core';
+
+
+@Component({
+  tag: 'page-infinite-scroll',
+  styleUrl: 'infinite-scroll.css'
+})
+export class PageInfiniteScroll {
+  @State() items: any[];
+
+  constructor() {
+    this.items = new Array(15).fill(0).map(() => {
+      return Math.random()
+    });
+  }
+
+  doInfinite(ev) {
+    setTimeout(() => {
+      console.log('hello world');
+
+      const newItems = new Array(15).fill(0).map(() => {
+        return Math.random()
+      });
+
+      this.items = this.items.concat(newItems);
+    }, 1000);
+    ev.target.complete();
+  }
+
+  render() {
+    return (
+      <ion-app>
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>Infinite Scroll</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+          <app-list items={this.items}></app-list>
+
+          <ion-infinite-scroll onIonInfinite={(event) => this.doInfinite(event)}>
+            <ion-infinite-scroll-content
+              loadingSpinner="bubbles"
+              loadingText="Loading more data...">
+            </ion-infinite-scroll-content>
+          </ion-infinite-scroll>
+        </ion-content>
+      </ion-app>
+    );
+  }
+}
