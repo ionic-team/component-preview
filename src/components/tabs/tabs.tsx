98c638e5f2cb696a76dacb365e55e0a246ec1436
jgw96 <jgw9617@gmail.com>
2018-06-13 14:25:45 -0500

    feat(): add tabs page

---
+++ src/components/tabs/tabs.tsx
@@ -0,0 +1,50 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+  tag: 'page-tabs',
+  styleUrl: 'tabs.css'
+})
+export class PageTabs {
+  render() {
+    return (
+      <ion-app>
+        <ion-tabs>
+          <ion-tab selected label="Music" icon='musical-note'>
+            <ion-header>
+              <ion-toolbar>
+                <ion-title>Music</ion-title>
+              </ion-toolbar>
+            </ion-header>
+
+            <ion-content padding>
+              <h1>Music</h1>
+            </ion-content>
+          </ion-tab>
+          <ion-tab label="Movies" icon='videocam'>
+            <ion-header>
+              <ion-toolbar>
+                <ion-title>Movies</ion-title>
+              </ion-toolbar>
+            </ion-header>
+
+            <ion-content padding>
+              <h1>Movies</h1>
+            </ion-content>
+          </ion-tab>
+          <ion-tab label="Games" icon='logo-game-controller-b'>
+            <ion-header>
+              <ion-toolbar>
+                <ion-title>Games</ion-title>
+              </ion-toolbar>
+            </ion-header>
+
+            <ion-content padding>
+              <h1>Games</h1>
+            </ion-content>
+          </ion-tab>
+        </ion-tabs>
+      </ion-app>
+    )
+  }
+}
