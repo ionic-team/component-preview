15369232e0c28777d7509a9f0aa1e44cfe05fa30
jgw96 <jgw9617@gmail.com>
2018-06-13 10:26:48 -0500

    feat(): add modal page

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,48 @@
+import { Component, Prop } from '@stencil/core';
+
+
+@Component({
+  tag: 'page-modal',
+  styleUrl: 'modal.css'
+})
+export class modal {
+  @Prop({ connect: 'ion-modal-controller' }) modalCtrl: HTMLIonModalControllerElement;
+
+  async openModal(char: number) {
+    const modal = await this.modalCtrl.create({
+      component: 'modal-page',
+      componentProps: { char }
+    });
+    modal.present();
+  }
+
+  render() {
+    return (
+      <ion-app>
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>Modal</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content padding>
+          <ion-list>
+            <ion-list-header>
+              Hobbits
+            </ion-list-header>
+
+            <ion-item onClick={() => this.openModal(0)}>
+              Gollum
+            </ion-item>
+            <ion-item onClick={() => this.openModal(1)}>
+              Frodo Baggins
+            </ion-item>
+            <ion-item onClick={() => this.openModal(2)}>
+              Sam
+            </ion-item>
+          </ion-list>
+        </ion-content>
+      </ion-app>
+    )
+  }
+}
