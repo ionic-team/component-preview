72952ef1a3d28ccad4abcb8d9eb49db7d24bd61b
jgw96 <jgw9617@gmail.com>
2018-06-13 09:24:53 -0500

    feat(): add spinner page

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,29 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+  tag: 'page-spinner',
+  styleUrl: 'spinner.css'
+})
+export class PageSpinner {
+
+  render() {
+    return (
+      <ion-app>
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>Spinner</ion-title>
+          </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+          <div id='centerBlock'>
+            <ion-spinner></ion-spinner>
+            <ion-spinner color='primary'></ion-spinner>
+            <ion-spinner color='secondary'></ion-spinner>
+          </div>
+        </ion-content>
+      </ion-app>
+    );
+  }
+}
