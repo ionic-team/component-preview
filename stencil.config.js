dee7005db84b5e1a18cea0b44a9c238a5b9d0b0a
jgw96 <jgw9617@gmail.com>
2018-06-25 13:56:18 -0500

    fix(): change docs-www url for the ionic-docs site

--- stencil.config.js
+++ stencil.config.js
@@ -2,7 +2,7 @@ exports.config = {
   outputTargets: [
     {
       type: 'www',
-      baseUrl: '/docs/content/component-preview-app/',
+      baseUrl: '/docs/content/component-preview-app/docs-www',
       serviceWorker: false,
       dir: 'docs-www'
     },
