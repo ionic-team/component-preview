4e4424c3ca0e8e5a8820e13ae546365452e2f55e
Brandy Carney <brandy@ionic.io>
2018-07-25 16:25:22 -0400

    chore(package): update ionic and stencil and add global css

--- stencil.config.js
+++ stencil.config.js
@@ -1,20 +1,18 @@
+// https://stenciljs.com/docs/config
+
 exports.config = {
+  globalStyle: 'src/global.css',
   outputTargets: [
+    {
+      type: 'www',
+      serviceWorker: false,
+      dir: 'www'
+    },
     {
       type: 'www',
       baseUrl: '/docs/content/component-preview-app/docs-www',
       serviceWorker: false,
       dir: 'docs-www'
-    },
-    {
-      type: 'www',
-      serviceWorker: false,
-      dir: 'www'
     }
   ]
-};
-
-exports.devServer = {
-  root: 'www',
-  watchGlob: '**/**'
 };
